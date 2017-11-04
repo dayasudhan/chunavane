3ce825617a89aae0e6b7d26d0ca9f771d1674f91
gagan s <gagan s>
2017-11-04 16:58:31 +0530

    kjljlkj

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -114,6 +114,48 @@ app = angular.module("adminModule", []);
         });
     };
 
+//////
+
+
+// from an input element
+// var filesToUpload = input.files;
+// var file = filesToUpload[0];
+
+// var img = document.createElement("img");
+// var reader = new FileReader();  
+// reader.onload = function(e) {
+//   img.src = e.target.result
+// }
+// reader.readAsDataURL(file);
+
+// var ctx = canvas.getContext("2d");
+// ctx.drawImage(img, 0, 0);
+
+// var MAX_WIDTH = 800;
+// var MAX_HEIGHT = 600;
+// var width = img.width;
+// var height = img.height;
+
+// if (width > height) {
+//   if (width > MAX_WIDTH) {
+//     height *= MAX_WIDTH / width;
+//     width = MAX_WIDTH;
+//   }
+// } else {
+//   if (height > MAX_HEIGHT) {
+//     width *= MAX_HEIGHT / height;
+//     height = MAX_HEIGHT;
+//   }
+// }
+// canvas.width = width;
+// canvas.height = height;
+// var ctx = canvas.getContext("2d");
+// ctx.drawImage(img, 0, 0, width, height);
+
+// var dataurl = canvas.toDataURL("image/png");
+
+
+/////
     $scope.addLogo = function (param,files) {
       console.log("addLogo");
       $scope.files = files;
@@ -145,10 +187,12 @@ app = angular.module("adminModule", []);
       };
      if($scope.files)
      {
-      fd.append("file",  $scope.files[0]);
-      fd.append("file",  $scope.files[1]);
+      for(var i = 0; i <$scope.files.length ; i++)
+      {
+        fd.append("file",  $scope.files[i]);
+      }
       console.log($scope.files);
-        }
+    }
      else
      {
       fd.append("file",  null);
